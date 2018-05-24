5e4f0f5db41253a136bdf10ecce2400ecda9a3ee
Hafiz <elkilic.h@hotmail.com>
2018-05-24 13:11:10 +0200

    ultrasonic + H-Brug

--- TopGear/TopGear.ino
+++ TopGear/TopGear.ino
@@ -9,29 +9,60 @@
 
 //Date: 24-05-2018
 
+//NEH: Nassim Emirhan Hafiz 
 
 
 
-bool enableLog;
+  bool enableLog;
 
-
-void setup()
+  //Wielen H-Brug
+  int aoNeg = 9;
+  int aoPos = 10;
+  
+  int outputValueNeg;
+  int outputValuePos;
+  int valueCount = 0;
+  bool moveUp = true;
+  int c_MaxValueInMeterRange = 255;
+  bool stopWheels = false;
+  
+void initPins()
 {
-  Serial.begin(115200);
-  Serial.println("init...");
-  initGlobals();
-}
+  Serial.println("init pins...");
 
-void loop()
-{
+  //Ultrasonic Sensor
+  #define echoPin 11 //Orange Cable
+  #define trigPin 12 //Yellow Cable
 
+  Serial.println("finished...");
 }
 
 void initGlobals()
 {
   Serial.println("init globals...");
+  
   enableLog = true;
+
+  //Ultrasonic Sensor
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
   Serial.println("finished...");
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("init...");
+  initPins();
+  initGlobals();
+}
+
+void loop()
+{
+  handleMovingCoilMeter();
+  //ultrasonicSensorNEH();
+  checkWall();
 }
 
 void debugLogging(String a_text, int a_infoType)
@@ -54,5 +85,85 @@ void debugLogging(String a_text, int a_infoType)
         break;
     }
   }
+}
+
+void ultrasonicSensorNEH() 
+{
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+}
+
+void loopMotor()
+{
+  if (valueCount < c_MaxValueInMeterRange && moveUp == true && stopWheels == false)
+  {
+    valueCount++;
+  }
+  else
+  {
+    moveUp = false;
+  }
+
+  if (valueCount > 0 && moveUp == false && stopWheels == false)
+  {
+    valueCount--;
+  }
+  else
+  {
+    moveUp = true;
+  }
+  
+  Serial.println("valueCount.." + (String)valueCount);
+  delay(10);
+  handleMovingCoilMeter();
+}
+
+void handleMovingCoilMeter()
+{
+
+}
+
+void checkWall()
+{
+
+  //outputValuePos = valueCount;
+  //outputValueNeg = 127; //c_MaxValueInMeterRange - valueCount;
+
+  
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(100);
+
+  if (distance < 8)
+  {
+    Serial.println("Wiel moet stoppen");
+    outputValueNeg = 0;
+    valueCount = 0;
+  }
+  else
+  {
+    outputValueNeg = 127;
+    valueCount = 50;
+  }
+
+    analogWrite(aoNeg, outputValuePos);
+    analogWrite(aoPos, outputValueNeg);
+ 
+  
 }
 

9f3817fb824dcd59f6ce67bdc38b78633e60c0a1
Hafiz <elkilic.h@hotmail.com>
2018-05-30 16:27:12 +0200

    .

--- TopGear/TopGear.ino
+++ TopGear/TopGear.ino
@@ -11,58 +11,123 @@
 
 //NEH: Nassim Emirhan Hafiz 
 
+  #include "SevSeg.h"
+  SevSeg sevseg; 
 
+
+  byte numDigits = 1;
+  byte digitPins[] = {};
+  byte segmentPins[] = {6, 5, 2, 3, 4, 7, 8, 9};
+  bool resistorsOnSegments = true;
 
   bool enableLog;
 
-  //Wielen H-Brug
-  int aoNeg = 9;
-  int aoPos = 10;
+  int aoNeg      =  2; // wiele
+  int aoPos      =  3; // wiele
+
+  int doStepPin0 =  8; // motor sensor
+  int doDirPin0  =  9; // motor sensor
+
+  #define echoPin   11 //Orange Cable
+  #define trigPin   12 //Yellow Cable
   
   int outputValueNeg;
   int outputValuePos;
   int valueCount = 0;
+  
   bool moveUp = true;
   int c_MaxValueInMeterRange = 255;
   bool stopWheels = false;
-  
+
+  int stepDelayInUs = 1500; //delay too short will stall motor sensor
+  int maxToeslag = 500;
+
 void initPins()
 {
   Serial.println("init pins...");
-
-  //Ultrasonic Sensor
-  #define echoPin 11 //Orange Cable
-  #define trigPin 12 //Yellow Cable
-
   Serial.println("finished...");
 }
 
 void initGlobals()
 {
   Serial.println("init globals...");
-  
-  enableLog = true;
-
-  //Ultrasonic Sensor
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  
   Serial.println("finished...");
 }
 
 void setup()
 {
+  
   Serial.begin(9600);
   Serial.println("init...");
   initPins();
   initGlobals();
+
+  pinMode(doDirPin0, OUTPUT);
+  pinMode(doStepPin0, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+  enableLog = true;
+
+  
 }
 
 void loop()
 {
-  handleMovingCoilMeter();
-  //ultrasonicSensorNEH();
   checkWall();
+}
+
+void ControlMotors() // sensor ultrasonic
+{
+  //SensorMotorLinksNEH();
+  //SensorMotorRechtsNEH();
+}
+
+void SensorMotorLinksNEH()
+{
+  digitalWrite(doDirPin0, HIGH);
+  for (int i = 0; i <= maxToeslag; i++)
+  {
+    digitalWrite(doStepPin0, LOW);
+    digitalWrite(doStepPin0, HIGH);
+    delayMicroseconds(stepDelayInUs);
+    if (i == maxToeslag)
+    {
+      digitalWrite(doDirPin0, LOW);
+      for (int i = 0; i <= maxToeslag; i++)
+      {
+        digitalWrite(doStepPin0, LOW);
+        digitalWrite(doStepPin0, HIGH);
+        delayMicroseconds(stepDelayInUs);
+      }
+    }
+  }
+}
+
+void SensorMotorRechtsNEH()
+{
+  digitalWrite(doDirPin0, LOW);
+  for (int i = 0; i <= maxToeslag; i++)
+  {
+    digitalWrite(doStepPin0, LOW);
+    digitalWrite(doStepPin0, HIGH);
+    delayMicroseconds(stepDelayInUs);
+    if (i == maxToeslag)
+    {
+//      if (distance > 8)
+//      {
+//         Serial.println("Wiel moet Rechts gaan");
+//      }
+      digitalWrite(doDirPin0, HIGH);
+      for (int i = 0; i <= maxToeslag; i++)
+      {
+        digitalWrite(doStepPin0, LOW);
+        digitalWrite(doStepPin0, HIGH);
+        delayMicroseconds(stepDelayInUs);
+      }
+    }
+  }
+  
 }
 
 void debugLogging(String a_text, int a_infoType)
@@ -86,57 +151,10 @@ void debugLogging(String a_text, int a_infoType)
     }
   }
 }
-
-void ultrasonicSensorNEH() 
-{
-  long duration, distance;
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  Serial.print(distance);
-  Serial.println(" cm");
-  delay(500);
-}
-
-void loopMotor()
-{
-  if (valueCount < c_MaxValueInMeterRange && moveUp == true && stopWheels == false)
-  {
-    valueCount++;
-  }
-  else
-  {
-    moveUp = false;
-  }
-
-  if (valueCount > 0 && moveUp == false && stopWheels == false)
-  {
-    valueCount--;
-  }
-  else
-  {
-    moveUp = true;
-  }
-  
-  Serial.println("valueCount.." + (String)valueCount);
-  delay(100);
-  handleMovingCoilMeter();
-}
-
-void handleMovingCoilMeter()
-{
-
-}
+ 
 
 void checkWall()
 {
-
-  //outputValuePos = valueCount;
-  //outputValueNeg = 127; //c_MaxValueInMeterRange - valueCount;
-
   
   long duration, distance;
   digitalWrite(trigPin, LOW);
@@ -147,23 +165,47 @@ void checkWall()
   distance = (duration/2) / 29.1;
   Serial.print(distance);
   Serial.println(" cm");
-  delay(100);
+  delay(250);
 
   if (distance < 8)
   {
     Serial.println("Wiel moet stoppen");
     outputValueNeg = 0;
     valueCount = 0;
+
+    //SensorMotorLinksNEH();
+      digitalWrite(doDirPin0, LOW);
+  for (int i = 0; i <= maxToeslag; i++)
+  {
+    digitalWrite(doStepPin0, LOW);
+    digitalWrite(doStepPin0, HIGH);
+    delayMicroseconds(stepDelayInUs);
+    if (i == maxToeslag)
+    {
+      if (distance > 10)
+      {
+         Serial.println("Wiel moet Rechts gaan");
+      }
+      digitalWrite(doDirPin0, HIGH);
+      for (int i = 0; i <= maxToeslag; i++)
+      {
+        digitalWrite(doStepPin0, LOW);
+        digitalWrite(doStepPin0, HIGH);
+        delayMicroseconds(stepDelayInUs);
+      }
+    }
+  }
   }
   else
   {
     outputValueNeg = 127;
     valueCount = 50;
+    ControlMotors();
   }
-
-  analogWrite(aoNeg, outputValuePos);
-  analogWrite(aoPos, outputValueNeg);
- 
   
+  analogWrite(aoNeg, valueCount);
+  analogWrite(aoPos, outputValueNeg);
+  
+ 
 }
 
